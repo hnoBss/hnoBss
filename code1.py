10b5f22d3f264f8a44e6f01d1d80c11f89846ac2
hnoBss <105928893+hnoBss@users.noreply.github.com>
2023-04-09 23:19:01 +0300

    testtt

--- code1.py
+++ code1.py
@@ -1,6 +1,6 @@
 
-numbers = [12, 65, 54, 39, 102, 339, 221,]
+number = [12, 65, 54, 39, 102, 339, 221,]
 
-result = list(filter(lambda x: (x % 13 == 0), numbers))
+result = list(filter(lambda x: (x % 13 == 0), number))
 
 print("Numbers divisible by 13 are",result)
